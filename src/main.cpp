e30c5c860fcf40871b3cb7498b21a2fc99d96660
Krzysztof 'RouNdeL' Zdulski <krzysztof.zdulski.stud@pw.edu.pl>
2020-03-13 12:12:02 +0100

    Create WebSocket -> UART forwarder

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,12 @@
 #include <ESPAsyncWebServer.h>
 #include <ESPAsyncWiFiManager.h>
 #include <ESP8266HTTPClient.h>
+#include <WebSocketsServer.h>
 
 #include "config.h"
 #include "communication.h"
 
-WiFiServer tcp(CONFIG_TCP_PORT);
+WebSocketsServer webSocket(81);
 
 AsyncWebServer server(80);
 DNSServer dns;
@@ -19,78 +20,38 @@ uint8_t buffer_size;
 
 uint8_t flags;
 
+void webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t length) {
+
+    switch (type) {
+        case WStype_CONNECTED: {
+            uint8_t resp[] = {0xA0};
+            webSocket.sendBIN(num, resp, 1);
+            break;
+        }
+        case WStype_BIN:
+            Serial.write(payload, length);
+            break;
+    }
+}
+
 void setup() {
     Serial.begin(9600);
-    tcp.begin();
 
     AsyncWiFiManager wifiManager(&server, &dns);
     wifiManager.setDebugOutput(true);
     wifiManager.autoConnect(CONFIG_AP_NAME);
+
+    webSocket.begin();
+    webSocket.onEvent(webSocketEvent);
 }
 
 void loop() {
-    uint8_t has_client = tcp.hasClient();
-    if(has_client) {
-        WiFiClient client = tcp.available();
-        if(client) {
+    webSocket.loop();
 
-            /*
-             * From time to time there are no bytes available, despite a client being.
-             * We wait up to 10ms for some to appear, if nothing appears, the client didn't send any data
-             */
-            for(uint8_t i = 0; i < 10; ++i) {
-                delay(1);
-                if(client.available())
-                    break;
-            }
-
-            while(client.available()) {
-                Serial.write(client.read());
-            }
-
-            /* Wait 100ms for serial data response*/
-            for(uint8_t i = 0; i < 100; ++i) {
-                delay(1);
-                if(Serial.available()) {
-                    break;
-                }
-            }
-
-            client.write(!Serial.available() ? TCP_ERROR_NO_RESPONSE : TCP_SUCCESS);
-
-            while(Serial.available()) {
-                client.write(Serial.read());
-            }
-
-            client.flush();
-            client.stop();
-        }
-    }
-
-    while(Serial.available()) {
-        if(flags & FLAG_UART_RECEIVING) {
-            if(buffer_index < buffer_size) {
-                uart_buffer[buffer_index] = Serial.read();
-                buffer_index++;
-            } else {
-                std::unique_ptr<BearSSL::WiFiClientSecure> client(new BearSSL::WiFiClientSecure);
-
-                client->setFingerprint(HTTP_SERVER_HTTPS_FINGERPRINT);
-                HTTPClient https;
-
-                if(https.begin(*client, String(HTTP_SERVER_HOST) + HTTP_SERVER_REPORT_URL)) {
-                    https.POST(uart_buffer, buffer_size);
-                    https.end();
-                }
-
-                flags &= ~FLAG_UART_RECEIVING;
-            }
-        } else {
-            buffer_size = Serial.read();
-            uart_buffer = (uint8_t *) malloc(buffer_size);
-            buffer_index = 0;
-
-            flags |= FLAG_UART_RECEIVING;
-        }
+    size_t size = (size_t) Serial.available();
+    if (size > 0) {
+        uint8_t response_buffer[size];
+        Serial.read((char *) response_buffer, size);
+        webSocket.broadcastBIN(response_buffer, size);
     }
 }
