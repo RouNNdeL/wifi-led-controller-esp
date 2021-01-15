46771f373ef5572b167c642d973b483fcbd04c93
Krzysztof Zdulski <krzys.zdulski@gmail.com>
2021-01-16 00:54:09 +0100

    Rework to support MQTT with Home Assistant

--- include/config.h
+++ include/config.h
@@ -5,10 +5,24 @@
 #ifndef ESP_CONFIG_H
 #define ESP_CONFIG_H
 
-#define BUFFER_SIZE 200
+#define UART_BUFFER_SIZE 200
 
 #ifndef CONFIG_AP_NAME
-#define CONFIG_AP_NAME "LED Controller"
+#define CONFIG_AP_NAME "Bed LEDs"
 #endif /* CONFIG_AP_NAME */
+
+// Keep these names as short as possible. No longer than 20-30 chars
+#define DEVICE_COUNT 3
+#define DEVICE_IDS {"bed_onboard", "bed_top","bed_bottom"}
+#define DEVICE_NAMES {"Onboard Led - Bed", "Top Bed Light", "Bottom Bed Light"}
+
+#define JSON_BUFFER_SIZE 256
+
+#define MQTT_BROKER_HOST "192.168.1.160"
+#define MQTT_BROKER_PORT 1883
+#define MQTT_USER "esp"
+#define MQTT_PASSWORD "password"
+#define MQTT_NODE_ID_PREFIX "esp_led"
+#define MQTT_DISCOVERY_PREFIX "homeassistant"
 
 #endif //ESP_CONFIG_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,53 @@
 #include <HardwareSerial.h>
 #include <ESPAsyncWebServer.h>
+#include <PubSubClient.h>
 #include <ESPAsyncWiFiManager.h>
 #include <WebSocketsServer.h>
+#include <ArduinoJson.h>
+#include <FastLED.h>
 
 #include <config.h>
 #include <uart.h>
 
-WebSocketsServer webSocket(81);
+#ifdef DEBUG_ESP_CUSTOM
+#define PRINTLN(a) Serial.println(a)
+#define PRINT(a) Serial.print(a)
+#else
+#define PRINTLN(a)
+#define PRINT(a)
+#endif // DEBUG_ESP_CUSTOM
+
+WiFiClient mqttTcpClient;
+PubSubClient mqtt(mqttTcpClient);
 
 AsyncWebServer server(80);
 DNSServer dns;
 
-uint8_t response_buffer[BUFFER_SIZE];
+char topicPrefix[100];
+char topicSetScan[100];
+
+const char *deviceNames[DEVICE_COUNT] = DEVICE_NAMES;
+const char *deviceIds[DEVICE_COUNT] = DEVICE_IDS;
+
+PROGMEM const char *discoveryTemplate = "{"
+                                        "\"~\":\"%s\","
+                                        "\"name\":\"%s\","
+                                        "\"unique_id\":\"%s\","
+                                        "\"cmd_t\":\"~/%s\","
+                                        "\"stat_t\":\"~/%s\","
+                                        "\"schema\":\"json\","
+                                        "\"brightness\": true,"
+                                        "\"rgb\":true"
+                                        "}";
+
+const char *topicConfigSuffix = "config";
+const char *topicSetSuffix = "set";
+const char *topicStateSuffix = "state";
+
+uint8_t response_buffer[UART_BUFFER_SIZE];
 uint8_t response_buffer_size = 0;
 uint8_t expected_response_size;
-
-void webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t length) {
-    switch (type) {
-        case WStype_CONNECTED: {
-            uint8_t resp[] = {(uint8_t) webSocket.connectedClients()};
-            webSocket.sendBIN(num, resp, 1);
-            break;
-        }
-        case WStype_BIN:
-            Serial.write(payload, length);
-            break;
-    }
-}
+bool uart_free;
 
 void clear_serial() {
     while (Serial.available()) {
@@ -34,36 +55,142 @@ void clear_serial() {
     }
 }
 
-void socket_send_data(uint8_t *data, size_t size) {
-    webSocket.broadcastBIN(data, size);
+void handle_data() {
+    uint8_t *data = response_buffer + 3;
+    switch (response_buffer[1]) {
+        case CMD_DEVICE_RESPONSE: {
+            uint8_t device_index = data[0];
+            uint8_t brightness = data[1];
+            uint8_t flags = data[2];
+            CRGB color(data[3], data[4], data[5]);
+            uint8_t effect = data[6];
+
+            StaticJsonDocument<JSON_BUFFER_SIZE> json;
+            json["state"] = flags & (1 << 0) ? "ON" : "OFF";
+            json["brightness"] = brightness;
+            json["color"]["r"] = color.r;
+            json["color"]["g"] = color.g;
+            json["color"]["b"] = color.b;
+
+            char topic[120];
+            snprintf(topic, sizeof(topic), "%s/%s", topicPrefix, deviceIds[device_index]);
+            strncat(topic, "/", sizeof(topic) - strlen(topic) - 1);
+            strncat(topic, topicStateSuffix, sizeof(topic) - strlen(topic) - 1);
+
+            char message[JSON_BUFFER_SIZE];
+            serializeJson(json, message);
+            bool success = mqtt.publish(topic, message, true);
+            if (!success) {
+                PRINT("Failed to publish state update for device ");
+                PRINTLN(deviceNames[device_index]);
+            } else {
+                PRINT("Published state update ");
+                PRINTLN(topic);
+            }
+        }
+    }
+}
+
+void uart_send(uint8_t *data, uint16_t size) {
+    for (uint16_t i = 0; i < size; ++i) {
+        if(Serial.available() && Serial.peek() == UART_BUSY) {
+            Serial.read();
+            uart_free = false;
+        }
+        while (!uart_free) {
+            if(Serial.available()) {
+                uart_free = true;
+            }
+        }
+        Serial.write(data[i]);
+    }
 }
 
 void handle_uart() {
     while (Serial.available()) {
         uint8_t byte = Serial.read();
         if (response_buffer_size == 0) {
+            if (byte == UART_BUSY) {
+                uart_free = false;
+            } else if (byte == UART_FREE) {
+                uart_free = true;
+            }
+
             if (byte != UART_BEGIN) {
                 clear_serial();
-                uint8_t data[] = {byte};
-                socket_send_data(data, sizeof(data));
                 return;
             }
         }
 
         response_buffer[response_buffer_size++] = byte;
-        if(response_buffer_size == 6) {
+        if (response_buffer_size == 3) {
             expected_response_size = byte;
         }
 
-        if(response_buffer_size == expected_response_size + 7) {
-            if(byte == UART_END) {
-                socket_send_data(response_buffer, response_buffer_size);
+        if (response_buffer_size == expected_response_size + 4) {
+            if (byte == UART_END) {
                 response_buffer_size = 0;
+                handle_data();
             } else {
                 clear_serial();
-                uint8_t data[] = {UART_INVALID_SEQUENCE};
-                socket_send_data(data, sizeof(data));
                 response_buffer_size = 0;
+            }
+        }
+    }
+}
+
+void requestAllDevices() {
+    uint8_t data[] = {UART_BEGIN, CMD_GET_ALL_DEVICES, 0, UART_END};
+    uart_send(data, sizeof(data));
+}
+
+void sendColor(uint8_t device_index, CRGB color) {
+    uint8_t data[] = {UART_BEGIN, CMD_SET_COLOR, 4, device_index, color.r, color.g, color.b, UART_END};
+    uart_send(data, sizeof(data));
+}
+
+void sendState(uint8_t device_index, bool state) {
+    uint8_t data[] = {UART_BEGIN, CMD_SET_STATE, 2, device_index, state, UART_END};
+    uart_send(data, sizeof(data));
+}
+
+void sendBrightness(uint8_t device_index, uint8_t brightness) {
+    uint8_t data[] = {UART_BEGIN, CMD_SET_BRIGHTNESS, 2, device_index, brightness, UART_END};
+    uart_send(data, sizeof(data));
+}
+
+void mqttCallback(char *topic, uint8_t *payload, uint16_t length) {
+    PRINT("Received payload for topic: ");
+    PRINTLN(topic);
+    char deviceId[32];
+    sscanf(topic, topicSetScan, deviceId);
+    PRINT("Device id: ");
+    PRINTLN(deviceId);
+    for (uint8_t i = 0; i < DEVICE_COUNT; ++i) {
+        if (!strcmp(deviceId, deviceIds[i])) {
+            StaticJsonDocument<JSON_BUFFER_SIZE> json;
+            deserializeJson(json, payload, DeserializationOption::NestingLimit(3));
+
+            if (json.containsKey("color") && json["color"].is<JsonObject>()) {
+                JsonObject color = json["color"].as<JsonObject>();
+                if (color.containsKey("r") && color.containsKey("g") && color.containsKey("b")
+                    && color["r"].is<uint8_t>() && color["g"].is<uint8_t>() && color["b"].is<uint8_t>()) {
+                    CRGB c = CRGB(color["r"], color["g"], color["b"]);
+                    sendColor(i, c);
+                }
+            }
+
+            if (json.containsKey("brightness") && json["brightness"].is<uint8_t>()) {
+                sendBrightness(i, json["brightness"]);
+            }
+
+            if (json.containsKey("state") && json["state"].is<char *>()) {
+                const char *state = json["state"];
+                if (!strcmp(state, "ON")) {
+                    sendState(i, true);
+                } else if (!strcmp(state, "OFF")) {
+                    sendState(i, false);
+                }
             }
         }
     }
@@ -73,15 +200,70 @@ void setup() {
     Serial.begin(BAUD);
 
     AsyncWiFiManager wifiManager(&server, &dns);
+#ifdef DEBUG_ESP_CUSTOM
     wifiManager.setDebugOutput(true);
+#endif // DEBUG_ESP_CUSTOM
     wifiManager.autoConnect(CONFIG_AP_NAME);
 
-    webSocket.begin();
-    webSocket.onEvent(webSocketEvent);
+    //mqttTcpClient.setInsecure();
+    mqtt.setServer(MQTT_BROKER_HOST, MQTT_BROKER_PORT);
+    mqtt.setCallback(mqttCallback);
+    mqtt.setBufferSize(512); // Temporally increase buffer size for discovery payloads
+
+    char nodeId[sizeof(MQTT_NODE_ID_PREFIX) + 16];
+    snprintf(nodeId, sizeof(nodeId), "%s_%08x", MQTT_NODE_ID_PREFIX, ESP.getChipId());
+    mqtt.connect(nodeId, MQTT_USER, MQTT_PASSWORD);
+
+    snprintf(topicPrefix, sizeof(topicPrefix), "%s/light/%s", MQTT_DISCOVERY_PREFIX, nodeId);
+    snprintf(topicSetScan, sizeof(topicSetScan), "%s/%%[^/]/%s", topicPrefix, topicSetSuffix);
+
+    char topic[120];
+    char deviceUid[sizeof(nodeId) + 32];
+    size_t jsonSize = strlen(discoveryTemplate) + 128;
+    char *discoveryJson = static_cast<char *>(malloc(jsonSize));
+
+    PRINTLN("Starting discovery");
+    for (uint8_t i = 0; i < DEVICE_COUNT; ++i) {
+        snprintf(topic, sizeof(topic), "%s/%s", topicPrefix, deviceIds[i]);
+        snprintf(deviceUid, sizeof(deviceUid), "%s_%s", nodeId, deviceIds[i]);
+        snprintf(discoveryJson, jsonSize, discoveryTemplate, topic, deviceNames[i],
+                 deviceUid, topicSetSuffix, topicStateSuffix);
+
+
+        bool success;
+        strncat(topic, "/", sizeof(topic) - strlen(topic) - 1);
+        strncat(topic, topicSetSuffix, sizeof(topic) - strlen(topic) - 1);
+        success = mqtt.subscribe(topic);
+        if (!success) {
+            PRINT("Failed to subscribe to topic for device ");
+            PRINTLN(deviceNames[i]);
+        } else {
+            PRINT("subscribed to topic ");
+            PRINTLN(topic);
+        }
+
+        // Undo concatenation
+        topic[strlen(topic) - strlen(topicSetSuffix)] = 0;
+
+        strncat(topic, topicConfigSuffix, sizeof(topic) - strlen(topic) - 1);
+        success = mqtt.publish(topic, discoveryJson, true);
+        if (!success) {
+            PRINT("Failed to publish discovery for device ");
+            PRINTLN(deviceNames[i]);
+        } else {
+            PRINT("Published discovery at ");
+            PRINTLN(topic);
+        }
+    }
+    mqtt.setBufferSize(256);
+    free(discoveryJson);
+
+    requestAllDevices();
 }
 
 void loop() {
-    webSocket.loop();
-
+    if (!mqtt.loop()) {
+        ESP.restart();
+    }
     handle_uart();
 }
